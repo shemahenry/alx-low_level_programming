b5e0b445b6875dbb4f4862b3650143fc7079328f
shemahenry <shemahenry777@gmail.com>
2022-04-12 17:26:57 +0000

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: combinations of numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
