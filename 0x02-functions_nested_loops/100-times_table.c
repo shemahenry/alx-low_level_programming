073b4f0d43413e199dba1904dd4dd25f285b6c37
shemahenry <shemahenry777@gmail.com>
2022-04-13 16:50:22 +0000

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,26 +33,7 @@ void print_times_table(int n)
 	_putchar((prod / 100) + '0');
 	_putchar(((prod / 10)) % 10 + '0');
 	}
-	else if (prod <= 99 && prod >= 10)
-	{
-		
-	for (mult = 1; mult <= num; mult++)
-	{
-	_putchar(',');
-	_putchar(' ');
 
-	prod = num * mult;
-
-	if (prod <= 99)
-	_putchar(' ');
-	if (prod <= 9)
-	_putchar(' ');
-
-	if (prod >= 100)
-	{
-	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10)) % 10 + '0');
-	}
 	else if (prod <= 99 && prod >= 10)
 	{
 	_putchar((prod / 10) + '0');
