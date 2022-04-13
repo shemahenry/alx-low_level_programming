d2a791fe541f242b05b343cd7ed70eecc93d8998
shemahenry <shemahenry777@gmail.com>
2022-04-13 01:39:10 +0000

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 
-	_putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
