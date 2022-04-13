ffe38541b17a5cc8422c8147d0add8931a4bc584
shemahenry <shemahenry777@gmail.com>
2022-04-13 18:16:01 +0000

    task16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,18 +17,18 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib1 % 10000000000;
+	fib1_half1 = fib1 / 1000000000;
+	fib2_half1 = fib2 / 1000000000;
+	fib1_half2 = fib1 % 1000000000;
+	fib2_half2 = fib1 % 1000000000;
 	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-		if ((fib1_half2 + fib2_half2) > 9999999999)
+		if ((fib1_half2 + fib2_half2) > 999999999)
 		{
 			half1 += 1;
-			half2 %= 10000000000;
+			half2 %= 1000000000;
 		}
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
