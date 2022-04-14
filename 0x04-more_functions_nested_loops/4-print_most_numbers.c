3f5a868a1932fd688dc60b4fda0839798ca6314e
shemahenry <shemahenry777@gmail.com>
2022-04-14 10:59:53 +0000

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,10 @@ void print_most_numbers(void)
 	i = 0;
 	while (i < 10)
 	{
-		if (i == 2 && i == 4)
+		if (i != 2 && i != 4)
 		{
-			continue;
+			_putchar (i + '0');
 		}
-		_putchar (i + '0');
 		i++;
 	}
 
